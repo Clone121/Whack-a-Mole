c415d540bc70e20f2f037c0802b75b964923ebda
Peter Lindblom <peter.lindblom01@gmail.com>
2023-06-12 08:33:27 +0000

    add js variables

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,10 @@
-console.log('hello');
+const boardBoxes = document.querySelectorAll('.boardBox');
+const timeLeft = document.querySelector('#time-left');
+const score = document.querySelector('#score');
+const startGame = document.querySelector('.start-game');
+
+let result = 0;
+let hit;
+let currentTime = 10;
+let countTime = null;
+let countDownTimer = null;
