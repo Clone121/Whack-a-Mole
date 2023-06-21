8272aea2305e6163429fd6214b3d1a9c97684f5c
clone121 <126593210+Clone121@users.noreply.github.com>
2023-06-21 15:20:49 +0200

    Update script.js
    Fixed time function stops on zero not one
    added disable true on start button for preventing bugs on game time
    added disable false after finished game to be able to play again

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,11 +6,23 @@ const startGame = document.querySelector('.start-game');
 let result = 0;
 let hit;
 let currentTime = 10;
-let countTime = null;
-let countDownTimer = null;
+let countTime;
+let countDownTimer;
 
+//Random function for mole
 
-//Random  game function
+function randomBoxes() {
+    boardBoxes.forEach((boardBox) => {
+        boardBox.classList.remove('mole');
+    });
+
+    let randomBoxes = boardBoxes[Math.floor(Math.random() * 9)];
+    randomBoxes.classList.add('mole');
+
+    hit = randomBoxes.id;
+}
+//Hit and score function
+
 function randomBoxes() {
     boardBoxes.forEach((boardBox) => {
         boardBox.classList.remove('mole');
@@ -22,7 +34,6 @@ function randomBoxes() {
     hit = randomBoxes.id;
 }
 
-//Hit and score function
 boardBoxes.forEach((boardBox) => {
     boardBox.addEventListener('mousedown', () => {
         if (boardBox.id == hit) {
@@ -36,20 +47,22 @@ boardBoxes.forEach((boardBox) => {
 function moveMole() {
     result = 0;
     currentTime = 10;
-    // Game speed
-    countTime = setInterval(randomBoxes, 1000);
+    countTime = setInterval(randomBoxes, 1000); //speed on mole
     countDownTimer = setInterval(countDown, 1000);
     timeLeft.textContent = currentTime;
     score.textContent = result;
 }
 
 function countDown() {
-    currentTime--;
-    timeLeft.textContent = currentTime;
-    if (currentTime === 0) {
+    if (currentTime > 0) {
+        currentTime--;
+        timeLeft.textContent = currentTime;
+        startGame.disabled = true;
+    } else {
         clearInterval(countDownTimer);
         clearInterval(countTime);
-        alert('GAME OVER! Your final score is : ' + result);
+        alert('Game Over! your score is : ' + result);
+        startGame.disabled = false;
     }
 }
 
