70cdcb2910ae1df1ab1fa07ff1f992d7d9ed58e8
clone121 <126593210+Clone121@users.noreply.github.com>
2023-06-27 18:13:55 +0200

    Update script.js
    fixed some bugs

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -11,7 +11,7 @@ let countDownTimer;
 
 //Random function for mole
 
-//function randomBoxes() {
+function randomBoxes() {
     boardBoxes.forEach((boardBox) => {
         boardBox.classList.remove('mole');
     });
